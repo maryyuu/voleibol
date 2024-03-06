fa475758c473d56df65087f8d3da7882206506f4
ricardo123nac <esteban1525marti@gmail.com>
2024-03-06 18:59:37 -0500

    cambios

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -2,6 +2,7 @@
 "use client"
 import { useState } from "react"
 import "./index.css"
+import { Noto_Sans_Tamil_Supplement } from "next/font/google"
 
 export default function Home() {
 
@@ -12,7 +13,7 @@ export default function Home() {
   var [Seet3, setSeet3] = useState(null)
   var [Seet4, setSeet4]= useState(null)
   var [Seet5, setSeet5] = useState(null)
-  var[empate, setEmpate] = useState(false)
+
   const Ganador = () => {
     const equipo1Sets = [Seet1, Seet2, Seet3, Seet4, Seet5].filter(set => set === "Equipo1").length;
     const equipo2Sets = [Seet1, Seet2, Seet3, Seet4, Seet5].filter(set => set === "Equipo2").length;
@@ -22,113 +23,87 @@ export default function Home() {
     } else if (equipo2Sets === 3) {
       return "Equipo 2";
     } else {
-      return null;
+      return null
+      
     }
   };
   
 
   const handlePuntosEquipo1 = () =>{ 
-    if (CountEquipo1 === 24 && CountEquipo2 === 24) {
-      // Si ambos equipos están empatados en 24, el conteo sigue
-      setCountEquipo1(CountEquipo1++);
-    // } else if (CountEquipo1 === 24 && CountEquipo2 < 24) {
-    //   // Si el equipo 1 tiene 24 puntos y el equipo 2 tiene menos de 24, el equipo 1 gana el set
-    //   setSeet1("Equipo1");
-    //   setCountEquipo1(0);
-    //   setCountEquipo2(0);
-    } else if (CountEquipo1 > 24 && CountEquipo1 - CountEquipo2 >= 2) {
-      // Si el equipo 1 tiene al menos 2 puntos más que el equipo 2, el equipo 1 gana el set
+    if (CountEquipo1 >= 24 && CountEquipo2 >= 24) {
+      // no entra a esta condición
+      setCountEquipo1(CountEquipo1+1);
+      setCountEquipo2(CountEquipo2+1)
+    } 
+    else if (CountEquipo1 > 24 && CountEquipo2 - CountEquipo1 >= 2) {
       setSeet1("Equipo1");
       setCountEquipo1(0);
       setCountEquipo2(0);
     } else {
-      // Incrementa el contador normalmente
-      setCountEquipo1(CountEquipo1 + 1);
-    }
-    
-    if(CountEquipo1 <= 25 ){
-      setCountEquipo1(CountEquipo1++)
-      
-    }else  {
-      if(!Seet1){
-        setCountEquipo1(0)
-        setCountEquipo2(0)
-      setSeet1("Equipo1")
-      }else if(!Seet2) {
-        setCountEquipo1(0)
-        setCountEquipo2(0)
-        setSeet2("Equipo1")
-      }else if(!Seet3){
-        setCountEquipo1(0)
-        setCountEquipo2(0)
-        setSeet3("Equipo1")
-      }else if(!Seet4){
-        CountEquipo1(0)
-        setCountEquipo2(0)
-        Seet4("Equipo1")
-      }else if(!Seet5){
-        CountEquipo1(0)
-        setCountEquipo2(0)
-        setSeet5("Equipo1")
+      if(CountEquipo1 <= 24 ){
+        setCountEquipo1(CountEquipo1+1) ;
+      }else  {
+       
+        if(!Seet1){
+          setCountEquipo2(0)
+          setCountEquipo1(0)
+        setSeet1("Equipo1")
+        }else if(!Seet2) {
+          setCountEquipo2(0)
+          setCountEquipo1(0)
+          setSeet2("Equipo1")
+        }else if(!Seet3){
+          setCountEquipo2(0)
+          setCountEquipo1(0)
+          setSeet3("Equipo1")
+        }else if(!Seet4){
+          setCountEquipo2(0)
+          setCountEquipo1(0)
+          setSeet4("Equipo2")
+        }else if(!Seet5){
+          setSeet5("Equipo1")
+        }
       }
-      
-      
-    }
-    
-     
-
+    }     
    }
 
   const handleMasEquipo2=()=>{
-    if (CountEquipo2 === 24 && CountEquipo1 === 24) {
-      // Si ambos equipos están empatados en 24, el conteo sigue
-      setCountEquipo2(CountEquipo2++);
-    } else if (CountEquipo2 === 24 && CountEquipo1 < 24) {
-      // Si el equipo 2 tiene 24 puntos y el equipo 1 tiene menos de 24, el equipo 2 gana el set
-      setSeet1("Equipo2");
-      setCountEquipo1(0);
-      setCountEquipo2(0);
-    } else if (CountEquipo2 > 24 && CountEquipo2 - CountEquipo1 >= 2) {
-      // Si el equipo 2 tiene al menos 2 puntos más que el equipo 1, el equipo 2 gana el set
+    if (CountEquipo1 === 24 && CountEquipo2 === 24) {
+      setCountEquipo2(CountEquipo2+1);
+      
+    }
+    else if (CountEquipo2 >24 && CountEquipo1 <= 25 ) {
       setSeet1("Equipo2");
       setCountEquipo1(0);
       setCountEquipo2(0);
     } else {
-      // Incrementa el contador normalmente
-      setCountEquipo2(CountEquipo2 + 1);
-    }
-    if(CountEquipo2 <= 25 ){
-      setCountEquipo2(CountEquipo2++) 
-    }else  {
-     
-      if(!Seet1){
-        setCountEquipo2(0)
-        setCountEquipo1(0)
-      setSeet1("Equipo2")
-      }else if(!Seet2) {
-        setCountEquipo2(0)
-        setCountEquipo1(0)
-        setSeet2("Equipo2")
-      }else if(!Seet3){
-        setCountEquipo2(0)
-        setCountEquipo1(0)
-        setSeet3("Equipo2")
-      }else if(!Seet4){
-        setCountEquipo2(0)
-        setCountEquipo1(0)
-        setSeet4("Equipo2")
-      }else if(!Seet5){
-        setSeet5("Equipo2")
+      if(CountEquipo2 <= 24 ){
+        setCountEquipo2(CountEquipo2+1) 
+      }else  {
+       
+        if(!Seet1){
+          setCountEquipo2(0)
+          setCountEquipo1(0)
+        setSeet1("Equipo2")
+        }else if(!Seet2) {
+          setCountEquipo2(0)
+          setCountEquipo1(0)
+          setSeet2("Equipo2")
+        }else if(!Seet3){
+          setCountEquipo2(0)
+          setCountEquipo1(0)
+          setSeet3("Equipo2")
+        }else if(!Seet4){
+          setCountEquipo2(0)
+          setCountEquipo1(0)
+          setSeet4("Equipo2")
+        }else if(!Seet5){
+          setSeet5("Equipo2")
+        }
       }
-      
-      
-    
     }
-    
-  
    }
-  //  const set1 =() =>{
- 
+
    const handleMenos1= () =>{
     setCountEquipo1(CountEquipo1-1)
    }
@@ -145,7 +120,7 @@ export default function Home() {
         <p class="textoEquipos">Equipo 1</p>
       <svg xmlns="http://www.w3.org/2000/svg" class="icon icon-tabler icon-tabler-shirt-filled" width="250" height="250" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" fill="none" stroke-linecap="round" stroke-linejoin="round"><path stroke="none" d="M0 0h24v24H0z" fill="none"/><path d="M14.883 3.007l.095 -.007l.112 .004l.113 .017l.113 .03l6 2a1 1 0 0 1 .677 .833l.007 .116v5a1 1 0 0 1 -.883 .993l-.117 .007h-2v7a2 2 0 0 1 -1.85 1.995l-.15 .005h-10a2 2 0 0 1 -1.995 -1.85l-.005 -.15v-7h-2a1 1 0 0 1 -.993 -.883l-.007 -.117v-5a1 1 0 0 1 .576 -.906l.108 -.043l6 -2a1 1 0 0 1 1.316 .949a2 2 0 0 0 3.995 .15l.009 -.24l.017 -.113l.037 -.134l.044 -.103l.05 -.092l.068 -.093l.069 -.08c.056 -.054 .113 -.1 .175 -.14l.096 -.053l.103 -.044l.108 -.032l.112 -.02z" stroke-width="0" fill="currentColor" /></svg>
        <div class="puntosIncrementados">
-    <button class="Puntos" onClick={ handlePuntosEquipo1} >
+    <button class="Puntos" onClick={handlePuntosEquipo1} >
            <svg xmlns="http://www.w3.org/2000/svg" class="icon icon-tabler icon-tabler-plus" width="20" height="20" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" fill="none" stroke-linecap="round" stroke-linejoin="round"><path stroke="none" d="M0 0h24v24H0z" fill="none"/><path d="M12 5l0 14" /><path d="M5 12l14 0" /></svg>
            </button>
            <button class="Puntos" onClick={handleMenos1}>
@@ -191,7 +166,7 @@ export default function Home() {
        </thead>
        <tbody>
         <tr>
-          <td class="tabla" >{  Seet1 }</td>
+          <td class="tabla" >{Seet1}</td>
           <td class="tabla">{Seet2}</td>
           <td class="tabla">{Seet3}</td>
           <td class="tabla">{Seet4}</td>
