2784a11928ca449cfaa08c24ca0bb611fa2827c2
maryyuu <charigdlopez@gmail.com>
2024-03-14 12:14:48 -0500

    Cmavio

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -14,15 +14,16 @@ export default function Home() {
   var [Seet4, setSeet4]= useState(null)
   var [Seet5, setSeet5] = useState(null)
 
+
+  
   const Ganador = () => {
     const equipo1Sets = [Seet1, Seet2, Seet3, Seet4, Seet5].filter(set => set === "Equipo1").length;
     const equipo2Sets = [Seet1, Seet2, Seet3, Seet4, Seet5].filter(set => set === "Equipo2").length;
 
-    if (equipo1Sets === 3) {
-      return "Equipo 1"
-      
+    if (equipo1Sets === 3 ) {
+      return "EQUIPO 1"
     } else if (equipo2Sets === 3) {
-      return "Equipo 2";
+      return "EQUIPO 2" 
     } else {
       return null
       
@@ -212,12 +213,25 @@ const handlePuntosEquipo1 = () => {
        <svg xmlns="http://www.w3.org/2000/svg" class="icon icon-tabler icon-tabler-minus" width="20" height="20" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" fill="none" stroke-linecap="round" stroke-linejoin="round"><path stroke="none" d="M0 0h24v24H0z" fill="none"/><path d="M5 12l14 0" /></svg>
          
         </button>
-        <span >
+        
+        </div>
+        <span class="Contador" >
         {CountEquipo1}
 
        </span>
         </div>
-        <div class="vs"><svg xmlns="http://www.w3.org/2000/svg" class="icon icon-tabler icon-tabler-vs" width="150" height="150" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" fill="none" stroke-linecap="round" stroke-linejoin="round"><path stroke="none" d="M0 0h24v24H0z" fill="none"/><path d="M2 12c0 5.523 4.477 10 10 10s10 -4.477 10 -10s-4.477 -10 -10 -10s-10 4.477 -10 10" /><path d="M14 14.25c0 .414 .336 .75 .75 .75h1.25a1 1 0 0 0 1 -1v-1a1 1 0 0 0 -1 -1h-1a1 1 0 0 1 -1 -1v-1a1 1 0 0 1 1 -1h1.25a.75 .75 0 0 1 .75 .75" /><path d="M7 9l2 6l2 -6" /></svg></div>
+        <div class="vs">
+          <svg xmlns="http://www.w3.org/2000/svg" class="icon icon-tabler icon-tabler-vs" width="150" height="150" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" fill="none" stroke-linecap="round" stroke-linejoin="round"><path stroke="none" d="M0 0h24v24H0z" fill="none"/><path d="M2 12c0 5.523 4.477 10 10 10s10 -4.477 10 -10s-4.477 -10 -10 -10s-10 4.477 -10 10" /><path d="M14 14.25c0 .414 .336 .75 .75 .75h1.25a1 1 0 0 0 1 -1v-1a1 1 0 0 0 -1 -1h-1a1 1 0 0 1 -1 -1v-1a1 1 0 0 1 1 -1h1.25a.75 .75 0 0 1 .75 .75" /><path d="M7 9l2 6l2 -6" /></svg>
+
+          <div class="set_reinicio">
+
+<section>
+<h1>Ganador</h1>
+  {Ganador()}
+  </section>
+<button>Reiniciar Partido</button>
+</div>
+        </div>
 
       
       
@@ -232,11 +246,13 @@ const handlePuntosEquipo1 = () => {
        <svg xmlns="http://www.w3.org/2000/svg" class="icon icon-tabler icon-tabler-minus" width="20" height="20" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" fill="none" stroke-linecap="round" stroke-linejoin="round"><path stroke="none" d="M0 0h24v24H0z" fill="none"/><path d="M5 12l14 0" /></svg>
          
         </button>
-        <span >
+       
+        
+    </div>
+    <span class="Contador" >
         {CountEquipo2}
 
        </span>
-        </div>
     </div>
     <div class="contenedorSets">
       <table class="">
@@ -260,11 +276,8 @@ const handlePuntosEquipo1 = () => {
        </tbody>
       </table>
     </div>
-    <div>
-      <h1>Ganador</h1>
-      <section>{Ganador()}</section>
-    </div>
-    </div>
+   
+    
 </div>
    
     </>
